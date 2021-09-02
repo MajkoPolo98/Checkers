db5d93b0efa8d7ff63d862e658965581c98c300c
Michal Pol <dissected9@gmail.com>
2021-09-03 01:10:55 +0200

    First commit

---
+++ src/test/java/com/project/checkers/CheckersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.checkers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
